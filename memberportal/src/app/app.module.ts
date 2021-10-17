e5064bfbf57f7b5a7ad3c59764e6856d38cd6756
snehase25 <snehajobs25@gmail.com>
2021-10-17 11:30:53 +0530

    Removed unused references from appmodule

--- memberportal/src/app/app.module.ts
+++ memberportal/src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MemberModule } from './member/member.module';
-import { SharedModule } from './shared/shared.module';
 
 
 
@@ -15,7 +13,6 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     MemberModule
   ],
   providers: [],
